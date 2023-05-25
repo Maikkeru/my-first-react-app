b4e04cc3779140db58f21a041b3dd170f0a14fc3
Michael Mitchell <nihongodehanaseru@yahoo.com>
2023-05-24 21:03:46 -0400

    Logic for the year selector

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,14 +3,16 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
-
-
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -19,15 +21,18 @@ function Expenses(props) {
           seleted={filteredYear}
           onChangeFilter={filterChangeHandler}
         ></ExpensesFilter>
-
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No Expenses Found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
